be2803db090068bc1d2a259cca84565e999d4e8d
Adrian <142854436+withyso@users.noreply.github.com>
2024-06-06 00:11:12 +0000

    validation login routes created

--- src/app.py
+++ src/app.py
@@ -6,10 +6,14 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -18,6 +22,10 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+#Obtener clave
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_KEY")
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -49,6 +57,31 @@ def handle_invalid_usage(error):
 
 # generate sitemap with all your endpoints
 
+@app.route('/api/login', methods=['POST'])
+def login(): 
+    body = request.get_json(silent=True)
+    if body is None: 
+        return jsonify({'msg' : 'You need to add info inside body'}), 400
+    if 'email' not in body:
+        return jsonify({'msg' : 'You need to add email info'}), 400
+    if 'password' not in body: 
+        return jsonify({'msg' : 'you need to add password info'}), 400
+    
+    user = User.query.filter_by(email=body['email']).all()
+    if len(user) == 0:
+        return jsonify({'msg' : 'username or password invalid'}), 400
+    if user[0].password != body['password']:
+        return jsonify({'msg' : 'username or password invalid'}), 400
+    
+    access_token = create_access_token(identity=user[0].email)
+    return jsonify({'msg' : 'ok', 'access_token' : access_token}), 200
+
+@app.route('/api/private', methods=['GET'])
+@jwt_required()
+def private():
+    current_user = get_jwt_identity()
+    print(current_user)
+    return jsonify({'msg' : 'This is a private message'})
 
 @app.route('/')
 def sitemap():
